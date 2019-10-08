645c61928f1b1b59dc895e754b854b3bc36e41bb
Daniel I <danielionita88@yahoo.com>
2019-10-08 02:54:10 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -41,7 +41,7 @@ def hit?(card_total)
   prompt_user
   user_input = get_user_input
   if user_input.to_s == "s"
-    number
+    card_total
   elsif user_input ==h 
   card_total += deal_card
   else invalid_command
