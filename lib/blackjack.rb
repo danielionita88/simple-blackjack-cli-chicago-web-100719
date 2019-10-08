827edd1bc6984ab857bdd1e5cc48bfa93b33428a
Daniel I <danielionita88@yahoo.com>
2019-10-08 02:36:07 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -22,8 +22,9 @@ def get_user_input
   user_input= gets.strip
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  # code #end_game here 
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
