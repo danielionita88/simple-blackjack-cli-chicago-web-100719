f88501b59e1bae27e6266e42cdaad68c12314ecd
Daniel I <danielionita88@yahoo.com>
2019-10-08 02:40:26 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,8 +31,8 @@ def initial_round
   # code #initial_round here
   num1 = deal_card
   num2 = deal_card
-  card_total = num1 + num2
-  display_card_total(card_total)
+  sum = num1 + num2
+  display_card_total(sum)
 end
 
 def hit?
