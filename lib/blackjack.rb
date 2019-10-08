dff4c0a8baa4b510e3405d2a2930d08f7209f7ae
Daniel I <danielionita88@yahoo.com>
2019-10-08 02:31:51 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -8,8 +8,8 @@ def deal_card
   rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(number)
+  puts "Your cards add up to #{number}"
 end
 
 def prompt_user
